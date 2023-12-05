342e42f19f0f6ceb164ca9514df13cebfe3f9a16
Andrew Basson <andrew.basson@olx.com>
2023-12-05 18:43:37 +0100

    chore(nvim): add additional languages

--- nvim/.config/nvim/lua/plugins/lang.lua
+++ nvim/.config/nvim/lua/plugins/lang.lua
@@ -1,4 +1,11 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 return {
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.terraform" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
 }
