5ec1091f578cfb2ebf0a23d36f103b51db204108
Andrew Basson <andrew.basson@olx.com>
2024-08-09 14:57:31 +0200

    chore(fzf): remove config from .zfz.zsh
    installed via asdf

--- zsh/.fzf.zsh
+++ zsh/.fzf.zsh
@@ -1,8 +1,9 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
-fi
+# needed when using homebrew
+#if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+#  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+#fi
 
 # Auto-completion
 # ---------------
