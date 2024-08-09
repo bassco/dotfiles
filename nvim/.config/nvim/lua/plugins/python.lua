eab3cd5703d295753779dadc4de1697edc3dede1
Andrew Basson <andrew.basson@olx.com>
2024-08-09 14:48:58 +0200

    chore(nvim): add language plugins and tmux integration

---
+++ nvim/.config/nvim/lua/plugins/python.lua
@@ -0,0 +1,11 @@
+-- stylua: ignore
+if true then return {} end
+
+-- return {
+--    {
+--       "williamboman/mason.nvim",
+--        opts = function(_, opts)
+--            table.insert(opts.ensure_installed, "black")
+--        end,
+--    },
+--}
