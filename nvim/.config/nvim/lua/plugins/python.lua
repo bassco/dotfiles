2be8c67304c9f0b3f30a4d9bb687e8e0fecb38d3
Andrew Basson <andrew.basson@gmail.com>
2026-03-01 22:22:14 +0100

    refactor(nvim): move plugin specs from options.lua to plugins/, migrate mason to mason-org

--- nvim/.config/nvim/lua/plugins/python.lua
+++ nvim/.config/nvim/lua/plugins/python.lua
@@ -3,7 +3,7 @@
 
 return {
     {
-       "williamboman/mason.nvim",
+       "mason-org/mason.nvim",
         opts = function(_, opts)
             table.insert(opts.ensure_installed, "black")
             table.insert(opts.ensure_installed, "isort")
---
+++ nvim/.config/nvim/lua/plugins/tools.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+        "cspell",
+      })
+    end,
+  },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters = {
+        markdownlint = {
+          args = { "--config", "~/.markdownlint.jsonc", "--" },
+        },
+      },
+    },
+  },
+}
