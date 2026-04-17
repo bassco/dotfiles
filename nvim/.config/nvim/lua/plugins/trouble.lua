f42b056d3d86631e6d9d32f85301346a7d47168f
Andrew Basson <andrew.basson@gmail.com>
2026-04-18 00:17:38 +0200

    chore(nvim): drop redundant trouble.nvim spec duplicate

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -1,12 +1,3 @@
 return {
-  -- change trouble config
-  {
-    "folke/trouble.nvim",
-    -- opts will be merged with the parent spec
-    opts = { use_diagnostic_signs = true },
-  },
-
-  -- disable trouble
-  { "folke/trouble.nvim", enabled = true },
-
+  { "folke/trouble.nvim", opts = { use_diagnostic_signs = true } },
 }
