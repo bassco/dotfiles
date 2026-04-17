8103f3f6e359879475aea9f7ed08895e7bfc27f0
Andrew Basson <andrew.basson@gmail.com>
2026-04-18 00:17:58 +0200

    style(nvim): reformat python.lua to 2-space indent per stylua.toml

--- nvim/.config/nvim/lua/plugins/python.lua
+++ nvim/.config/nvim/lua/plugins/python.lua
@@ -1,12 +1,9 @@
--- stylua: ignore
--- if true then return {} end
-
 return {
-    {
-       "mason-org/mason.nvim",
-        opts = function(_, opts)
-            table.insert(opts.ensure_installed, "black")
-            table.insert(opts.ensure_installed, "isort")
-        end,
-    },
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "black")
+      table.insert(opts.ensure_installed, "isort")
+    end,
+  },
 }
