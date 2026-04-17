3ef28794e25e165487eba215ceddf458ac163a93
Andrew Basson <andrew.basson@gmail.com>
2026-04-17 23:11:46 +0200

    feat(nvim): add blink.cmp override with enter preset

---
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        menu = { auto_show = true },
+        ghost_text = { enabled = false },
+        accept = { auto_brackets = { enabled = true } },
+        list = { selection = { preselect = false, auto_insert = false } },
+      },
+      keymap = {
+        preset = "enter",
+      },
+    },
+  },
+}
