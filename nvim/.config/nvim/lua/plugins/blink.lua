2ac88afd4148d2d47402ce424935d582fcc510f5
Andrew Basson <andrew.basson@gmail.com>
2026-04-17 23:46:27 +0200

    feat(nvim): enable blink.cmp ghost text

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -4,7 +4,7 @@ return {
     opts = {
       completion = {
         menu = { auto_show = true },
-        ghost_text = { enabled = false },
+        ghost_text = { enabled = true },
         accept = { auto_brackets = { enabled = true } },
         list = { selection = { preselect = false, auto_insert = false } },
       },
