6f6aa41c7c22fb61738b19e425b465e1bc0f0926
Andrew Basson <andrew.basson@gmail.com>
2026-04-17 23:56:46 +0200

    fix(nvim): register custom jsonls schemas as list items + nest validate under settings.json

--- nvim/.config/nvim/lua/plugins/json.lua
+++ nvim/.config/nvim/lua/plugins/json.lua
@@ -27,20 +27,26 @@ return {
           on_new_config = function(new_config)
             new_config.settings.json.schemas = new_config.settings.json.schemas or {}
             vim.list_extend(new_config.settings.json.schemas, require("schemastore").json.schemas())
-            vim.list_extend(new_config.settings.json.schemas.extra, {
-              name = "Gitlab CI",
-              description = "Gitlab CI files",
-              fileMatch = { "gitlab-ci/*.yml", ".gitlab-ci.yml", ".gitlab/*.yml" },
-              url = "https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json",
-            })
-            vim.list_extend(new_config.settings.json.schemas.extra, {
-              name = "Helm Unittest",
-              description = "Helm Unittest files",
-              fileMatch = { "tests/*_test.yaml" },
-              url = "https://raw.githubusercontent.com/helm-unittest/helm-unittest/main/schema/helm-testsuite.json",
+            vim.list_extend(new_config.settings.json.schemas, {
+              {
+                name = "Gitlab CI",
+                description = "Gitlab CI files",
+                fileMatch = { "gitlab-ci/*.yml", ".gitlab-ci.yml", ".gitlab/*.yml" },
+                url = "https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json",
+              },
+              {
+                name = "Helm Unittest",
+                description = "Helm Unittest files",
+                fileMatch = { "tests/*_test.yaml" },
+                url = "https://raw.githubusercontent.com/helm-unittest/helm-unittest/main/schema/helm-testsuite.json",
+              },
             })
           end,
-          validate = { enable = true },
+          settings = {
+            json = {
+              validate = { enable = true },
+            },
+          },
         },
       },
     },
